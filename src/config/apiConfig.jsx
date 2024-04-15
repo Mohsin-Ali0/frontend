e368f551cd922fa84246310471fb53e9de40c4d5
Mohsin-Ali0 <mohsinali.brainwave@gmail.com>
2024-04-16 02:17:40 +0500

    url updated

--- src/config/apiConfig.jsx
+++ src/config/apiConfig.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // axios.defaults.baseURL = "http://localhost:3004/";
-axios.defaults.baseURL = "https://3452-58-65-197-79.ngrok-free.app";
+axios.defaults.baseURL = "https://72d5-58-65-197-79.ngrok-free.app";
 // axios.defaults.baseURL = "https://dev74.onlinetestingserver.com/acolai/api";
 
 // let user = (localStorage.getItem('user'));
