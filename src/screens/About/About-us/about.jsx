dc40d3077c6bb2713f1fa5cfe4a3b009ed62028a
Mohsin-Ali0 <160528279+Mohsin-Ali0@users.noreply.github.com>
2024-11-06 01:56:12 +0500

    Update about.jsx

--- src/screens/About/About-us/about.jsx
+++ src/screens/About/About-us/about.jsx
@@ -1,11 +1,20 @@
-import React, { useRef, useState } from 'react';
+import React, { useRef, useState } from "react";
 import "./About.css";
-import { AboutUS, AboutSection2, AboutSliderImg1, AboutSliderImg2, ModalIcon, ReviewsLogo, PaymentSuccessIcon } from '../../../assets/images';
-import { Image } from 'react-bootstrap';
-import { Swiper, SwiperSlide } from 'swiper/react';
+import {
+  AboutUS,
+  AboutSection2,
+  AboutSliderImg1,
+  AboutSliderImg2,
+  ModalIcon,
+  ReviewsLogo,
+  PaymentSuccessIcon,
+} from "../../../assets/images";
+import { Image } from "react-bootstrap";
+import { Swiper, SwiperSlide } from "swiper/react";
 import { Pagination, Autoplay } from "swiper/modules";
 
 export default function About() {
+  usePageTitle("About Us");
   const [activeIndex, setActiveIndex] = useState(0);
   const swiperRef = useRef(null);
 
@@ -28,10 +37,10 @@ export default function About() {
         <div className="container">
           <div className="row">
             <div className="col-md-6 d-flex align-items-center">
-              <h2 className='about-text pb-5'>About Us</h2>
+              <h2 className="about-text pb-5">About Us</h2>
             </div>
             <div className="col-md-6">
-              <Image src={AboutUS} className='img-fluid' />
+              <Image src={AboutUS} className="img-fluid" />
             </div>
           </div>
         </div>
@@ -40,22 +49,28 @@ export default function About() {
         <div className="row About-Section-1">
           <div className="col-md-6 d-flex align-items-center">
             <div>
-              <h3 className='about-text-2'>Exceptional designs for your <br /> exceptional ideas</h3>
+              <h3 className="about-text-2">
+                Exceptional designs for your <br /> exceptional ideas
+              </h3>
               <div className="row mt-5">
                 <div className="col-md-6">
-                  <h5 className='about-text-3'>01</h5>
-                  <p className='about-text-4'>Cursus vitae congue mauris <br /> rhoncus. Amet est placerat in</p>
+                  <h5 className="about-text-3">01</h5>
+                  <p className="about-text-4">
+                    Cursus vitae congue mauris <br /> rhoncus. Amet est placerat
+                    in
+                  </p>
                 </div>
                 <div className="col-md-6">
-                  <h5 className='about-text-3'>02</h5>
-                  <p className='about-text-4'>Pharetra magna ac placerat <br /> vestibulum lectus.</p>
+                  <h5 className="about-text-3">02</h5>
+                  <p className="about-text-4">
+                    Pharetra magna ac placerat <br /> vestibulum lectus.
+                  </p>
                 </div>
               </div>
             </div>
           </div>
           <div className="col-md-6 d-flex justify-content-center">
-
-            <Image src={AboutSection2} className='img-fluid About-second-img' />
+            <Image src={AboutSection2} className="img-fluid About-second-img" />
           </div>
         </div>
       </div>
@@ -63,22 +78,33 @@ export default function About() {
         <div className="row About-Section-2">
           <div className="col-md-6">
             <div className="About-Before-text d-flex">
-<h2>M</h2>
-              <p className='about-text-4 mt-2 ms-2'>in nulla posuere sollicitudin aliquam ultrices sagittis. Quisque id <br />diam vel quam elementum pulvinar. Mi proin sed libero enim </p>
+              <h2>M</h2>
+              <p className="about-text-4 mt-2 ms-2">
+                in nulla posuere sollicitudin aliquam ultrices sagittis. Quisque
+                id <br />
+                diam vel quam elementum pulvinar. Mi proin sed libero enim{" "}
+              </p>
             </div>
 
-            <p className='about-text-4 About-text-8'>Sed faucibus turpis. Porttitor lacus luctus. Elementum nibh tellus molestie nunc non blandit massa. Malesuada fames ac turpis egestas.</p>
+            <p className="about-text-4 About-text-8">
+              Sed faucibus turpis. Porttitor lacus luctus. Elementum nibh tellus
+              molestie nunc non blandit massa. Malesuada fames ac turpis
+              egestas.
+            </p>
           </div>
           <div className="col-md-6">
-            <p className='about-text-4'>Cursus vitae congue mauris rhoncus. Amet est placerat in egestas erat. Pharetra magna ac placerat vestibulum lectus. Porttitor lacus luctus accumsan tortor posuere ac ut.</p>
+            <p className="about-text-4">
+              Cursus vitae congue mauris rhoncus. Amet est placerat in egestas
+              erat. Pharetra magna ac placerat vestibulum lectus. Porttitor
+              lacus luctus accumsan tortor posuere ac ut.
+            </p>
           </div>
         </div>
       </div>
       <div className="container">
         <div className="row About-Section-3">
-          <section className='About-slider-container'>
-
-          <Swiper
+          <section className="About-slider-container">
+            <Swiper
               spaceBetween={30}
               slidesPerView={2} // Default view: 2 slides per view
               slidesPerGroup={1} // Slide one image at a time
@@ -112,20 +138,17 @@ export default function About() {
                   spaceBetween: 30,
                 },
                 // when window width is >= 1024px
-             
               }}
             >
               {AboutImages.map((item, index) => (
                 <SwiperSlide key={item.id}>
                   <div className="slider-image-container">
-                    <Image src={item.img} className='About-slider-img' />
+                    <Image src={item.img} className="About-slider-img" />
                   </div>
                 </SwiperSlide>
               ))}
             </Swiper>
           </section>
-
-
         </div>
       </div>
     </React.Fragment>
