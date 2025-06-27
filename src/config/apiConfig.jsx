491ec3acca1a013deaca96d63f694e90f5da4b0b
Mohsin-Ali0 <mohsinali.brainwave@gmail.com>
2025-06-28 03:07:24 +0500

    production url fixes

--- src/config/apiConfig.jsx
+++ src/config/apiConfig.jsx
@@ -1,8 +1,8 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "http://localhost:3004/";
+// axios.defaults.baseURL = "http://localhost:3004/";
 // axios.defaults.baseURL = "https://cd6b-58-65-197-79.ngrok-free.app";
-// axios.defaults.baseURL = "https://backend.vidtrial.com";
+axios.defaults.baseURL = "https://backend.vidtrial.com";
 // axios.defaults.baseURL = procees.env.ASDNJSAKDNBKJSA
 
 let token = localStorage.getItem("token");
